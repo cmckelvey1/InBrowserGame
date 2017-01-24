5a2e27b9672fd45af35918de9e3eb97fd6e7a41a
McKelvey, Corridon (cm0441) <cm0441@att.com>
2017-01-24 11:57:50 -0600

    I haved added player input and some monsters

---
+++ playerInput.js
@@ -0,0 +1,11 @@
+//HANDLES PLAYER INPUT
+
+var keysDown = {};
+
+addEventListener("keydown", function(e){
+	keysDown[e.keyCode] = true;
+}, false);
+
+addEventListener("keyup", function(e){
+	delete keysDown[e.keyCode];
+}, false);
