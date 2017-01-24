2f4a556f135720951c0c0f285a4c680391db14b3
McKelvey, Corridon (cm0441) <cm0441@att.com>
2017-01-24 11:50:55 -0600

    I HAVE ADDED BACKGROUND IMAGES AND A CANVAS TO PLACE THAT IMAGE ON

---
+++ canvas.js
@@ -0,0 +1,8 @@
+// CREATE CANVAS
+var canvas = document.createElement("canvas");
+var ctx = canvas.getContext("2d");
+
+canvas.width = 512;
+canvas.height = 480;
+
+document.body.appendChild(canvas);
---
+++ createBackground.js
@@ -0,0 +1,8 @@
+// BACKGROUND 
+var bgReady = false;
+var bgImage = new Image();
+bgImage.onload = function(){
+	bgReady = true;
+};
+
+bgImage.src = "./imgs/GameBackground.jpg";
